895d323e1d54be46bbf0370b35de4306ae8eeafe
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-08-06 19:52:45 +0900

    $table에 이벤트 추가(이벤트 버블링) 다시 체크

--- main.js
+++ main.js
@@ -6,19 +6,18 @@ const $result = document.createElement('div');
 const rows = [];
 
 let turn = 'O';
+const callback = (event) => {
+  if (event.target.textContent) return;
+  // event.target이 곧 $td를 의미한다. 가급적 event.target을 쓰는 게 좋다!
+  // 정확히는 event.currentTarget을 해주는 게 더 정확한 표현!
+  event.target.textContent = turn;
+  turn = turn === 'O' ? 'X' : 'O';
+};
 for (let i = 0; i < 3; i++) {
   const $tr = document.createElement('tr');
   const cells = [];
   for (let j = 0; j < 3; j++) {
     const $td = document.createElement('td');
-
-    $td.addEventListener('click', (event) => {
-      console.log('clicked');
-      if (event.target.textContent) return;
-      // event.target이 곧 $td를 의미한다. 가급적 event.target을 쓰는 게 좋다!
-      event.target.textContent = turn;
-      turn = turn === 'O' ? 'X' : 'O';
-    });
 
     cells.push($td);
     $tr.append($td);
@@ -26,4 +25,12 @@ for (let i = 0; i < 3; i++) {
   rows.push(cells);
   $table.append($tr);
 }
+$table.addEventListener('click', callback);
+// 다시 체크하기(버블링 파트)
+$table.addEventListener('click', (event) => {
+  console.log(event.target);
+  console.log(event.currentTarget);
+});
+
 body.append($table);
+body.append($result);
